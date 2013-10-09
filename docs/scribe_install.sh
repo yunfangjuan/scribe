5d81ce303b54f511eaf7d699c6a4cc4ec5b77017
Yun-Fang Juan <yunfangjuan@gmail.com>
2013-10-09 00:38:15 +0000

    scribe installation guide

---
+++ docs/scribe_install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# pre-requisites
+sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev 
+
+#python nodejs depenmdencies
+sudo apt-get  install automake libboost-test-dev python-dev python-twisted
+sudo apt-get  install nodejs nodejs-dev
+sudo apt-get  install ibglib2.0-dev
+
+
+#javac
+sudo apt-get install openjdk-7-jdk 
+# boost 1.36.0
+wget http://sourceforge.net/projects/boost/files/boost/1.40.0/boost_1_40_0.tar.gz/download -O boost_1_40_0.tar.gz 
+tar xvfz boost_1_40_0.tar.gz 
+cd boost_1_40_0; ./configure; jam; sudo jam install; cd ..;
+
+#download thrift
+wget http://mirror.cogentco.com/pub/apache/thrift/0.9.1/thrift-0.9.1.tar.gz 
+tar xvfz thrift-0.9.1.tar.gz 
+cd thrift-0.9.1; ./configure --with-boost=/usr/local; make; sudo make install; cd ../;
+# install fb303
+cd thrift-0.9.1/contrib/fb303; ./configure --with-boost=/usr/local; make; sudo make install; cd ../../..; 
+
+#download scribe
+git clone https://github.com/yunfangjuan/scribe
+cd scribe; ./boostrap.sh; ./configure --with-boost=/usr/local --with-boost-system=boost_system-gcc46-mt --with-boost-filesystem=boost_filesystem; make; sudo make install  
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/lib:/usr/lib:/usr/local/lib
