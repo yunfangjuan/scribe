d569fdef39be51e0593ba0ea3565a3ae31fcd07a
Yun-Fang Juan <yunfangjuan@gmail.com>
2013-10-11 00:40:22 +0000

    example scribe configs and more readable documentation

--- docs/scribe_install.sh
+++ docs/scribe_install.sh
@@ -19,12 +19,20 @@ cd boost_1_40_0; ./configure; jam; sudo jam install; cd ..;
 #download thrift
 wget http://mirror.cogentco.com/pub/apache/thrift/0.9.1/thrift-0.9.1.tar.gz 
 tar xvfz thrift-0.9.1.tar.gz 
-cd thrift-0.9.1; ./configure --with-boost=/usr/local; make; sudo make install; cd ../;
+cd thrift-0.9.1; ./configure --with-boost=/usr/local; make; sudo make install; 
+# install thrift python packages
+cd lib/py; sudo python setup.py install
+cd ../../;
 # install fb303
-cd thrift-0.9.1/contrib/fb303; ./configure --with-boost=/usr/local; make; sudo make install; cd ../../..; 
+cd contrib/fb303; ./configure --with-boost=/usr/local; make; sudo make install; 
+#instal fb303 packages
+cd py; sudo python setup.py install
+cd ../../../../;
 
 #download scribe
 git clone https://github.com/yunfangjuan/scribe
 cd scribe; ./boostrap.sh; ./configure --with-boost=/usr/local --with-boost-system=boost_system-gcc46-mt --with-boost-filesystem=boost_filesystem; make; sudo make install  
-
+# install scribe python lib
+cd lib/py; sudo python setup.py install
+cd ../../../
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/lib:/usr/lib:/usr/local/lib
